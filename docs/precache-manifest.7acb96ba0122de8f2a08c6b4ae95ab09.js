0cf78f10aae19a81832a893f14efb74bdd62a87e
Junior Medrano <junior.med19@gmail.com>
2020-08-13 12:17:24 -0500

    docs agregado

---
+++ docs/precache-manifest.7acb96ba0122de8f2a08c6b4ae95ab09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f84dbb19575d44e7dd334d472280d04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b402ffd0e2801bb8473",
+    "url": "/static/css/main.8b1fa2c3.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b402ffd0e2801bb8473",
+    "url": "/static/js/main.bf37c658.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
